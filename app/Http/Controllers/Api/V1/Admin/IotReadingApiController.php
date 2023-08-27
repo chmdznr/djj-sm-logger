e9ff1741a393db629a2b628e5db70119bfcefd80
fidojahfalprayoga <fidojahfalprayoga@gmail.com>
2023-08-27 15:39:18 +0700

    Initial swagger install

--- app/Http/Controllers/Api/V1/Admin/IotReadingApiController.php
+++ app/Http/Controllers/Api/V1/Admin/IotReadingApiController.php
@@ -13,6 +13,30 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IotReadingApiController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/iot_reading",
+     *     summary="Get iot_reading data",
+     *     tags={"IOT Reading"},
+     *     @OA\Response(response="200", description="Success",
+     *     @OA\JsonContent(
+     *         type="object",
+     *         @OA\Property(property="data", type="array",
+     *          @OA\Items(
+     *            type="object",
+     *            @OA\Property(property="id", type="integer", example=1),
+     *            @OA\Property(property="respondent_id", type="integer", example=1),
+     *            @OA\Property(property="respondent_name", type="string", example="Feliona"),
+     *            @OA\Property(property="fetal_hr", type="integer", example=1),
+     *            @OA\Property(property="resp_count", type="integer", example=1),
+     *            @OA\Property(property="created_at", type="string", example="2023-08-24 02:06:10"),
+     *            
+     *          )
+     *         )
+     *     )
+     * ),
+     *   ),
+     */
     public function index()
     {
         abort_if(Gate::denies('iot_reading_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,29 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Pregnancy Api",
+ *     version="1.0.0",
+ *     description="Deskripsi API"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     description="Login with email and password to get the authentication token. 
+ *     Input the token without 'Bearer' prefix.",
+ *     in="header",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth",
+ *     name="Authorization",
+ * )
+ * 
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Pregnancy API Server"
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
