28d622fdb442294385050c81df0af26155062ad6
Kyroline <rizky.w.dewantoro@gmail.com>
2023-08-27 21:28:41 +0700

    Swagger Docs Added

--- app/Http/Controllers/Api/V1/Admin/IotReadingApiController.php
+++ app/Http/Controllers/Api/V1/Admin/IotReadingApiController.php
@@ -15,7 +15,7 @@ class IotReadingApiController extends Controller
 {
     /**
      * @OA\Get(
-     *     path="/iot_reading",
+     *     path="/iot-readings",
      *     summary="Get iot_reading data",
      *     tags={"IOT Reading"},
      *     @OA\Response(response="200", description="Success",
@@ -25,11 +25,30 @@ class IotReadingApiController extends Controller
      *          @OA\Items(
      *            type="object",
      *            @OA\Property(property="id", type="integer", example=1),
-     *            @OA\Property(property="respondent_id", type="integer", example=1),
-     *            @OA\Property(property="respondent_name", type="string", example="Feliona"),
-     *            @OA\Property(property="fetal_hr", type="integer", example=1),
-     *            @OA\Property(property="resp_count", type="integer", example=1),
+     *            @OA\Property(property="fetal_hr", type="integer", example=99),
+     *            @OA\Property(property="resp_count", type="integer", example=9),
      *            @OA\Property(property="created_at", type="string", example="2023-08-24 02:06:10"),
+     *            @OA\Property(property="updated_at", type="string", example="2023-08-24 02:06:10"),
+     *            @OA\Property(property="deleted_at", type="string", example=null),
+     *            @OA\Property(property="responden_id", type="integer", example=1),
+     *         @OA\Property(property="responden", type="array",
+     *          @OA\Items(
+     *            type="object",
+     *            @OA\Property(property="id", type="integer", example=1),
+     *            @OA\Property(property="nama", type="string", example="Valerica"),
+     *            @OA\Property(property="kode", type="string", example="ES101"),
+     *            @OA\Property(property="usia", type="integer", example=29),
+     *            @OA\Property(property="his_adekuat", type="string", example="0"),
+     *            @OA\Property(property="pergerakan", type="string", example="0"),
+     *            @OA\Property(property="paritas", type="integer", example=null),
+     *            @OA\Property(property="kardiotokografi", type="integer", example=null),
+     *            @OA\Property(property="alamat", type="string", example="Chateau de Versaille"),
+     *            @OA\Property(property="created_at", type="string", example="2023-08-24 02:06:10"),
+     *            @OA\Property(property="updated_at", type="string", example="2023-08-24 02:06:10"),
+     *            @OA\Property(property="deleted_at", type="string", example=null),
+     *            
+     *          )
+     *         )
      *            
      *          )
      *         )
